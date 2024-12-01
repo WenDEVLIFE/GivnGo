716d71d54f144b1aa4e84c0852b73353e9f42c97
Frouen M. Medina Jr. <117834496+WenDEVLIFE@users.noreply.github.com>
2024-12-01 13:31:52 +0800

    Delete app/givngo_codes directory

--- app/givngo_codes/src/main/java/com/go/givngo/Model/DonorDetails.kt
+++
@@ -1,9 +0,0 @@
-package com.go.givngo.Model
-
-data class DonorDetails(
-    val organizationName: String = "",
-    val statusAccount: String = "",
-    val firstName: String = "",
-    val lastName: String = "",
-    val uriProfileImage: String = ""
-)
--- app/givngo_codes/src/main/java/com/go/givngo/Model/SealedImageLoad.kt
+++
@@ -1,8 +0,0 @@
-package com.go.givngo.Model
-
-
-sealed class ImageLoadState {
-    object Success : ImageLoadState()
-    object Error : ImageLoadState()
-    object Loading : ImageLoadState()
-}
